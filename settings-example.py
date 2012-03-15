33f21f29110cadc64d0d3f6c3e1b82c9d0f2e44b
Bryan Kendall <bryan.a.kendall@gmail.com>
2012-03-15 04:39:01 -0400

    added south to the settings example

--- settings-example.py
+++ settings-example.py
@@ -125,6 +125,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    # Database migration framework
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
