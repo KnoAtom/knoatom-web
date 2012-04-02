4d243208534cc603ca05c5f762b0e05ef4d7c28d
Bryan Kendall <bryan.a.kendall@gmail.com>
2012-04-02 10:44:29 -0400

    added help text for forms, video id

--- web/forms/submission.py
+++ web/forms/submission.py
@@ -10,4 +10,4 @@ def validate_youtube_video_id(value):
 class SubmissionForm(forms.Form):
     title = forms.CharField(max_length=100, required=True)
     content = forms.CharField(widget=forms.Textarea, required=True)
-    video = forms.CharField(max_length=100, validators=[validate_youtube_video_id])
+    video = forms.CharField(max_length=100, validators=[validate_youtube_video_id], help_text='Please enter a 11 character YouTube video id.')
