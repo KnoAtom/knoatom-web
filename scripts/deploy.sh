03dd796248706ab453d57eee0f6032abae1e1f4e
Bryan Kendall <bkend@umich.edu>
2012-05-27 17:58:05 -0400

    added deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ../
+
+# move stuff to web directory
+cp -R * /var/www/knoatom-web/
+
+# remove .git directory from deployment
+rm -rf /var/www/knoatom-web/.git
+
+cd /var/www/knoatom-web
+
+# collect static stuff
+python manage.py collectstatic --noinput
+
+# sync db
+python manage.py syncdb --noinput
+
+# restart apache
+service apache2 restart
