add6ebfd0d86a08190b229a4c9fa84e7699adc96
Bryan Kendall <bryan@bryankendall.com>
2012-06-02 22:08:12 -0500

    moved things around to make it more like the 1.4 documentation

--- knoatom/settings-example.py
+++ knoatom/settings-example.py
@@ -1,4 +1,4 @@
-# Django settings for knoatom-web project.
+# Django settings for web project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -7,19 +7,19 @@ ADMINS = (
     ('Bryan Kendall', 'bkend@umich.edu'),
 )
 
+MANAGERS = ADMINS
+
 EMAIL_SUBJECT_PREFIX = '[KnoAtom] '
 
 SERVER_EMAIL = 'knoatom-noreply@umich.edu'
 
-MANAGERS = ADMINS
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'db_name',                      # Or path to database file if using sqlite3.
-        'USER': 'db_username',                      # Not used with sqlite3.
+        'USER': 'db_user',                      # Not used with sqlite3.
         'PASSWORD': 'db_password',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -44,32 +44,30 @@ SITE_ID = 1
 USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
+# calendars according to the current locale.
 USE_L10N = True
+
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/path/to/knoatom-static/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://localhost/knoatom-static/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/path/to/static/'
+STATIC_ROOT = '/path/to/knoatom-static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://static.example.com/'
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = ''
+STATIC_URL = 'http://localhost/knoatom-static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -87,7 +85,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = '&ebsvca#*1q4ja!4voz%(u)#flb#bn+q+@qs*kpl6+=bf8wkb&'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -102,19 +100,19 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-
-    'knoatom-web.web.middleware.SecureRequiredMiddleware.SecureRequiredMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'knoatom-web.urls'
+ROOT_URLCONF = 'knoatom.urls'
 
-from os import path
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'web.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    path.join(path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -135,15 +133,21 @@ INSTALLED_APPS = (
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
+# the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
@@ -156,6 +160,18 @@ LOGGING = {
     }
 }
 
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+)
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -166,12 +182,10 @@ HTTPS_SUPPORT = False
 SECURE_REQUIRED_PATHS = (
     '/login',
     '/account',
-    '/admin',
 )
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^login/?$', 'web.views.account.login', name='login'),
@@ -23,12 +19,6 @@ urlpatterns = patterns('',
     url(r'^category/(\d+)/?$', 'web.views.home.category', name='category'),
     url(r'^expos/(\d+)/?$', 'web.views.home.expos', name='expos'),
     url(r'^$', 'web.views.home.index', name='home'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 
     url(r'^mu-25b8a55c-a9fee579-723dcc44-9782bfc2$', 'web.views.blitz.index'),
     url(r'^google3da26e317e8e8cda.html$', 'web.views.google_webmaster.index'),
